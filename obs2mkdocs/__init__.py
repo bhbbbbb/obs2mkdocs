0371ee01e7d51c683ff70b26ca9a0d3fce1299be
bhbbbbb <smark9005310@gmail.com>
2022-11-09 21:40:25 +0800

    fix bugs

---
+++ obs2mkdocs/__init__.py
@@ -0,0 +1,1 @@
+from .coverter import convert, convert_dir
--- obs2mkdocs/math_block.py
+++ obs2mkdocs/math_block.py
@@ -6,10 +6,13 @@ from .base import IterBuff, ParsingException
 logger = logging.getLogger(__name__)
 
 MATH_BLOCK_WRAP = [r'\$\$', r'\$\$']
-# LIST_START_PATTERN = r'^\s*[\+\-\*]'
+LIST_START_PATTERN = r'^\s*[\+\-\*]'
 
 def fix(fiter: IterBuff, fout: TextIO):
-    """_summary_
+    """fix math block format
+
+    1. pymarkdown only accept math block with both blankline at preceding and following lines.
+    2. blankline in block is not allowed.
 
     Args:
         fiter (IterBuff): from the line before opening to the line at closing.
@@ -23,29 +26,28 @@ def fix(fiter: IterBuff, fout: TextIO):
     if fiter.lookahead is None:
         return
 
-    # if re.search(LIST_START_PATTERN, fiter.lookahead):
-    #     return
-    
     if re.search(MATH_BLOCK_WRAP[0], fiter.lookahead) is None:
         return
-
-    match = re.search(r'^([>\s]*)' + MATH_BLOCK_WRAP[0], fiter.lookahead)
-    if match is None:
-        raise ParsingException(
-            f'Unsupported Format for content "{fiter.lookahead}" at line {fiter.idx + 1}'
-        )
     
-    indent = match[1]
+    is_not_list = re.search(LIST_START_PATTERN, fiter.lookahead) is None
+
+    if is_not_list:
+        match = re.search(r'^([>\s]*)' + MATH_BLOCK_WRAP[0], fiter.lookahead)
+        if match is None:
+            raise ParsingException(
+                f'Unsupported Format for content "{fiter.lookahead}" at line {fiter.idx + 1}'
+            )
+    
+    indent = match[1] if is_not_list else ''
     blankline = f'{indent}\n'
     blankline_pattern = r'\s*' + indent.strip() + r'\s+$'
 
-    if re.match(blankline_pattern, fiter.cur) is None: # if not blank
+    if is_not_list and re.match(blankline_pattern, fiter.cur) is None: # if not blank
         fout.write(blankline)
         logger.info(f'inserted blank line before block at line {fiter.idx}')
 
     fout.write(fiter.next().cur)
     opening_line = fiter.idx
-
 
     # check is not one-line block. e.g. $$equations$$
     if re.search(f'{MATH_BLOCK_WRAP[0]}.*?{MATH_BLOCK_WRAP[1]}', fiter.cur) is None:
@@ -53,7 +55,10 @@ def fix(fiter: IterBuff, fout: TextIO):
         # find closing
         while True:
             try:
-                fout.write(fiter.next().cur)
+                ## delete blankline
+                if next(fiter).strip() == '':
+                    continue
+                fout.write(fiter.cur)
                 if re.search(MATH_BLOCK_WRAP[1], fiter.cur):
                     break
             except StopIteration:
@@ -63,7 +68,13 @@ def fix(fiter: IterBuff, fout: TextIO):
                     f' , but closing symbol \'{MATH_BLOCK_WRAP[1]}\' not found until line {fiter.idx}.'
                 )
 
-    if fiter.lookahead is None or re.match(blankline_pattern, fiter.lookahead) is None:
+    if (
+        is_not_list 
+        and (
+            fiter.lookahead is None
+            or re.match(blankline_pattern, fiter.lookahead) is None
+        )
+    ):
         fout.write(blankline)
         logger.info(f'inserted blankline after block at line {fiter.idx}')
     
